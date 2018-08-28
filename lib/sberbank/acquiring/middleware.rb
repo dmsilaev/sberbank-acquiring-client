f5d48e515cfe7ee98675d4bdad5fd963d1c3903c
dmsilaev <dmitry.silaev@overteam.ru>
2018-08-28 11:42:46 +0300

    remove flatten

--- lib/sberbank/acquiring/middleware.rb
+++ lib/sberbank/acquiring/middleware.rb
@@ -4,13 +4,11 @@ module Sberbank
   module Acquiring
     module Middleware
       autoload :ParseJson, 'sberbank/acquiring/middleware/response/parse_json'
-      autoload :FlattenJson, 'sberbank/acquiring/middleware/response/flatten_json'
       autoload :HandleError, 'sberbank/acquiring/middleware/response/handle_error'
 
       if Faraday::Middleware.respond_to?(:register_middleware)
         Faraday::Response.register_middleware \
           :json   => lambda { ParseJson },
-          :flatten => lambda { FlattenJson },
           :error  => lambda { HandleError }
       end
     end
